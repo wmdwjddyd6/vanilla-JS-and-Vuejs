59d367a477bf67778e11f5bc8cd2cf1754a2abb8
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-06-26 07:09:02 +0900

    Tab 구현
    추천 검색어, 최근 검색어 Tab 선택까지 구현

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,5 +1,6 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
+import TabView from '../views/TabView.js'
 
 import SearchModel from '../models/SearchModel.js'
 
@@ -12,7 +13,19 @@ export default {
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
 
+        TabView.setup(document.querySelector('#tabs'))
+            .on('@change', e => this.onChangeTab(e.detail.tabName))
+
         ResultView.setup(document.querySelector('#search-result'))
+
+        this.selectedTab = '추천 검색어'
+        this.renderView()
+    },
+
+    renderView() {
+        console.log(tag, 'renderView()')
+        TabView.setActiveTab(this.selectedTab)
+        ResultView.hide()
     },
 
     search(query){
@@ -34,5 +47,9 @@ export default {
 
     onSearchResult(data){
         ResultView.render(data)
+    },
+
+    onChangeTab(tabName){
+        debugger
     }
 }
