6eddd87d8da13c215cac0ef9af6a2d74bbabd331
BlueEar <67060302+wmdwjddyd6@users.noreply.github.com>
2021-06-26 06:18:07 +0900

    SearchResult 구현
    검색결과 구현

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,8 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+
+import SearchModel from '../models/SearchModel.js'
+
 const tag = '[MainController]'
 
 export default {
@@ -7,13 +11,28 @@ export default {
         FormView.setup(document.querySelector('form'))
             .on('@submit', e => this.onSubmit(e.detail.input))
             .on('@reset', e => this.onResetForm())
+
+        ResultView.setup(document.querySelector('#search-result'))
+    },
+
+    search(query){
+        console.log(tag, 'search()', query)
+        SearchModel.list(query).then(data => {
+            this.onSearchResult(data)
+        })
     },
 
     onSubmit(input){
         console.log(tag, 'onSubmit()', input)
+        this.search(input)
     },
 
     onResetForm(){
         console.log(tag, 'onResetForm()')
+        ResultView.hide()
+    },
+
+    onSearchResult(data){
+        ResultView.render(data)
     }
 }
--- 1-vanilla/js/models/SearchModel.js
+++ 1-vanilla/js/models/SearchModel.js
@@ -1,13 +1,13 @@
 const data = [
   {
     id: 1,
-    name: '[키친르쎌] 홈메이드 칠리소스 포크립 650g',
-    image: 'https://cdn.bmf.kr/_data/product/H1821/5a4ed4e8a6751cb1cc089535c000f331.jpg'
+    name: '스테이크',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo1WHofNnyl4Rqk8LKoaS_w_Zb1tJroBrsB7MwySqxqKJAMlWRIqlPDHwUjCU&usqp=CAc'
   },
   {
     id: 2,
-    name: '[키친르쎌] 이탈리아 파티 세트 3~4인분',
-    image: 'https://cdn.bmf.kr/_data/product/H503E/300d931e3b8252ed628b6a3c2f56936b.jpg'
+    name: '파스타',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIl_wvIJ6vHhoCrO-DowD_z__Mv7H-csYu9Q&usqp=CAU'
   }
 ]
 
